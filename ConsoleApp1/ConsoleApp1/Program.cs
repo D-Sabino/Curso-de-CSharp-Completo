355f382d989c9535c2c56d97c14f1436179b8e18
Daniel Sabino <dannisabino17@gmail.com>
2023-03-28 19:56:37 -0300

    Variáveis e seus tipos

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Danki.Code!");
+            //TipoDessaVariavel nomeParaEssaVariavel = atribuirUmValor
+
+            /*
+                int - armazenam numeros inteiros. Ex: 10, 15, 20, 30 ...
+                float - armazenam numeros quebrados. Ex: 20.5, 24.8...
+                string - armazenam um texto
+                char - armazenam um caractere
+                bool - booleano (verdadeiro ou falso)
+            */
+
+            //Exemplo de declaração, Int:
+            int numero = 10;
+
+            //Float:
+            float numero_2 = 2.5f;
+
+            //String:
+            string nome = "Daniel";
+
+            //Char:
+            char caractere = 'D';
+
+            //Exibindo o valor preenchido no console.
+            Console.WriteLine(nome);
+            Console.WriteLine(numero);
         }
     }
 }

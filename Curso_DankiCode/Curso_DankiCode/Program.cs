b4ae237639f23f8a3c596c46844894b678389faa
Daniel Sabino <dannisabino17@gmail.com>
2023-04-03 16:40:55 -0300

    Metodos

--- Curso_DankiCode/Curso_DankiCode/Program.cs
+++ Curso_DankiCode/Curso_DankiCode/Program.cs
@@ -3,6 +3,21 @@ using System.Runtime.CompilerServices;
 
 namespace Curso_DankiCode
 {
+    /* Aula:  Metodos */
+    abstract class Motocicleta
+    {
+        //Public, pode ser chamado por qualquer metodo.
+        public void LigarMoto() { }
+
+        //Protected, apenas classe derivadas pode chamar.
+        protected void AdicionarGasolina(int galoes) { }
+
+        public abstract double ChegarNaVelocidadeX();
+
+        public virtual int Drive(int quilometros, int velocidade) { }
+    }
+
+
     /* Aula:  Criando classes no C# */
     public class Aluno
     {
@@ -20,8 +35,6 @@ namespace Curso_DankiCode
         public string nomeDoAluno { get; set; }
     }
 
-
-
     class Program
     {
         static void Main(string[] args)
@@ -30,6 +43,13 @@ namespace Curso_DankiCode
             Aluno aluno2 = new Aluno("Bianca");
             Console.WriteLine(aluno1.nomeDoAluno);
             Console.WriteLine(aluno2.nomeDoAluno);
+
+
+
+
+
+
+
         }
     }
 }

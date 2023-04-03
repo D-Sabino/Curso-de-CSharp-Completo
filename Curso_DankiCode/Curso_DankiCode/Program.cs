a58aa3eb9921a4cbbf2432c5444e5350a6c2c9fb
Daniel Sabino <dannisabino17@gmail.com>
2023-04-02 23:30:27 -0300

    Criando classes no C#

---
+++ Curso_DankiCode/Curso_DankiCode/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Curso_DankiCode
+{
+    /* Aula:  Criando classes no C# */
+    public class Aluno
+    {"
+        public string nome = "Daniel", cpf = "99999999999";
+        public int nota_anual = 10;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.WriteLine(aluno.nome);
+        }
+    }
+}

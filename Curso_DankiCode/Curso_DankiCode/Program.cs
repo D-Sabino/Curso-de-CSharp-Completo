f145d071cfa4f64c22e7c55c3b9c54f742e2415d
Daniel Sabino <dannisabino17@gmail.com>
2023-04-02 23:35:01 -0300

    Construtor

--- Curso_DankiCode/Curso_DankiCode/Program.cs
+++ Curso_DankiCode/Curso_DankiCode/Program.cs
@@ -1,21 +1,35 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace Curso_DankiCode
 {
     /* Aula:  Criando classes no C# */
     public class Aluno
-    {"
+    {
+        /*
         public string nome = "Daniel", cpf = "99999999999";
         public int nota_anual = 10;
+        */
+
+        /* Aula:  Construtor */
+        public Aluno(string nome)
+        {
+            nomeDoAluno = nome;
+        }
+
+        public string nomeDoAluno { get; set; }
     }
+
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            Aluno aluno = new Aluno();
-
-            Console.WriteLine(aluno.nome);
+            Aluno aluno1 = new Aluno("Daniel");
+            Aluno aluno2 = new Aluno("Bianca");
+            Console.WriteLine(aluno1.nomeDoAluno);
+            Console.WriteLine(aluno2.nomeDoAluno);
         }
     }
 }

f504876301b93e44a52b836de053b4d4d3b5fa3c
Daniel Sabino <dannisabino17@gmail.com>
2023-04-05 07:24:57 -0300

    Aula: Conhecendo o Try e o Catch

--- Danki-Bank/Danki-Bank/Program.cs
+++ Danki-Bank/Danki-Bank/Program.cs
@@ -17,6 +17,7 @@ namespace Danki_Bank
             Numero = numero;
 
             /* Aula: Tratando nosso primeiro erro*/
+            /*
             if (TotalContasCriadas == 0)
             {
                 TaxaOperacao = 30;
@@ -27,19 +28,35 @@ namespace Danki_Bank
                 TaxaOperacao = 30 / (TotalContasCriadas + 1);
                 TotalContasCriadas++;
             }
-            
-        }
+            */
+
+            /* Aula: Conhecendo o Try e o Catch */
+            try
+            {
+                TotalContasCriadas++;
+                TaxaOperacao = 30 / TotalContasCriadas;
+                Console.WriteLine("O resultado é:" + TaxaOperacao);
+            }
+            catch(DivideByZeroException)
+            {
+                Console.WriteLine("Não podemos dividir por zero!");
+            }
+        }   
     }
 
     class Progam
     {
         static void Main(string[] args)
         {
+            /* Aula: Tratando nosso primeiro erro*/
             ContaCorrente conta = new ContaCorrente(9999, 123456);
             ContaCorrente conta2 = new ContaCorrente(8888, 654321);
-            
+
+            /*
             Console.WriteLine(ContaCorrente.TotalContasCriadas);
             Console.WriteLine(ContaCorrente.TaxaOperacao);
+            */
+
             Console.ReadLine();
         }
     }

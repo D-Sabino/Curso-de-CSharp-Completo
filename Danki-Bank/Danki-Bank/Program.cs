5be534e35230bc8bae8efd474c86d64f07c6789c
Daniel Sabino <dannisabino17@gmail.com>
2023-04-10 22:51:20 -0300

    Aula: Conhecendo os arrays

--- Danki-Bank/Danki-Bank/Program.cs
+++ Danki-Bank/Danki-Bank/Program.cs
@@ -3,6 +3,7 @@
 namespace Danki_Bank
 {
     /* Aula: Criando nosso primeiro erro */
+    /*
     public class ContaCorrente
     {
         private int Agencia, Numero;
@@ -10,14 +11,14 @@ namespace Danki_Bank
         public static int TotalContasCriadas { get; private set; }
 
 
-        
+        /*
         public ContaCorrente(int agencia, int numero)
         {
-            Agencia = agencia;
-            Numero = numero;
 
             /* Aula: Tratando nosso primeiro erro*/
             /*
+            Agencia = agencia;
+            Numero = numero;
             if (TotalContasCriadas == 0)
             {
                 TaxaOperacao = 30;
@@ -31,6 +32,7 @@ namespace Danki_Bank
             */
 
             /* Aula: Conhecendo o Try e o Catch */
+            /*
             try
             {
                 TotalContasCriadas++;
@@ -41,23 +43,41 @@ namespace Danki_Bank
             {
                 Console.WriteLine("Não podemos dividir por zero!");
             }
+            
         }   
     }
-
+    */
     class Progam
     {
         static void Main(string[] args)
         {
             /* Aula: Tratando nosso primeiro erro*/
+            /*
             ContaCorrente conta = new ContaCorrente(9999, 123456);
             ContaCorrente conta2 = new ContaCorrente(8888, 654321);
-
+            */
             /*
             Console.WriteLine(ContaCorrente.TotalContasCriadas);
             Console.WriteLine(ContaCorrente.TaxaOperacao);
+            Console.ReadLine();
             */
 
-            Console.ReadLine();
+            /* Aula: Conhecendo os arrays*/
+            /*
+            int[] matriz;
+            matriz = new int[10];
+            */
+
+            string[] nomes;
+            nomes = new string[5];
+
+            nomes[0] = "Joana";
+            nomes[1] = "Roberto";
+            nomes[2] = "Daniel";
+            nomes[3] = "Lucas";
+            nomes[4] = "Bianca";
+
+            Console.WriteLine(nomes[4]);
         }
     }
 }

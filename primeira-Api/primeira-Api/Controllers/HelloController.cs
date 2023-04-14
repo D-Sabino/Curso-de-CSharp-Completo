8ae33ce10aed2e3fb49953501c193af1d6ecf1fe
Daniel Sabino <dannisabino17@gmail.com>
2023-04-14 07:52:33 -0300

    Aula: Criando um novo controlador e entendendo as rotas

---
+++ primeira-Api/primeira-Api/Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace primeira_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        /* Tipos de interaçoes:
+            Post
+            Get
+            Put
+            Delete
+        */
+
+        public IActionResult Get()
+        {
+            return Ok("Ola mundo!\nEstou aprendendo a construir minha primeira API.");
+        }
+
+    }
+}
